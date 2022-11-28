7b7bf31c8e50dab70523f565a8fb54d05b7f7faf
Anna Smolskaya <anna31rus@mail.ru>
2022-11-28 23:50:53 +0300

    fixed popup

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,25 @@
-console.log('Hello')
-
 // сохраняем ссылки на нужные элементы в переменные JS
+const editButton = document.querySelector('.profile__btn-edit');
+const popup = document.querySelector('.popup');
+const popupClose = document.querySelector('.popup__btn-close');
 
+
+// вешаем обработчик на клик по кнопке, в скобках пишем (событие, функция)
+editButton.addEventListener('click', openEditPopup) 
+popupClose.addEventListener('click', closeEditPopup)
+
+// функция открытия попапа
+function openEditPopup() {
+  popup.classList.add('popup_opened');
+}
+// функция удаляет окно открытого попапа
+function closeEditPopup() {
+  popup.classList.remove('popup_opened');
+}
+
+
+
+
+
+
+console.log({editButton, popup, popupClose});
